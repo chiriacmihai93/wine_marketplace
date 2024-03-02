d4ff74b7d2698b1562bccb7bf4b5f3dc2b7f7cc2
Chiriac Mihai <145295885+chiriacmihai93@users.noreply.github.com>
2024-03-02 12:30:01 +0200

    Update urls.py

--- profile/urls.py
+++ profile/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import profile_redirect
 from profile.views import profile
 from App_marketplace.views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('profile/', profile_redirect, name='profile_redirect'),
     path('profile/', profile, name='profile'),
 ]
