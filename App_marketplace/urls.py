aa79cb48977096697f16e01c3683379848360ac7
Chiriac Mihai <145295885+chiriacmihai93@users.noreply.github.com>
2024-03-02 18:09:33 +0200

    Update urls.py
    rezolvate erori atunci cand nu e autentificat utilizatorul

--- App_marketplace/urls.py
+++ App_marketplace/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-#from . import views
+from account.views import LoginView
 from .views import home_view, contact, ProductListView, product_detail, upload_product, add_to_cart, delete_product, shopping_cart, finalizeaza_comanda
 
 #in cadrul acestui fisier vom defini lista de app pointuri a aplicatiei
@@ -8,6 +7,7 @@ app_name = 'App_marketplace'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
     path('upload_product/', upload_product, name='upload-product'),
     path('products/', ProductListView.as_view(), name='products'),
     path('product/<int:product_id>/', product_detail, name='product'),
